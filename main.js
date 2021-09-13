8885b1a895098221b6f49fe95df35d500da2927b
Ken Kieu <kenkieu93@gmail.com>
2021-09-13 15:44:09 -0700

    Retrieve form data, and add to localStorage.

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+var data = {
+  view: 'entry-form',
+  days: {
+    sunday: [],
+    monday: [],
+    tuesday: [],
+    wednesday: [],
+    thursday: [],
+    friday: [],
+    saturday: []
+  },
+  editing: null,
+  nextEntryId: 1
+};
+
+var previousDataJSON = localStorage.getItem('schedule-entry');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+function handleAddLocalStorage(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('schedule-entry', dataJSON);
+}
+
+window.addEventListener('beforeunload', handleAddLocalStorage
+);
+
 var $eventForm = document.querySelector('#event-form');
 var $submitButton = document.querySelector('#submit-button');
 
@@ -6,7 +34,8 @@ function handleAddEntry(event) {
 
   var formData = {};
   formData.time = $eventForm.time.value;
-  console.log('value of formData.time:', formData.time);
+  formData.description = $eventForm.description.value;
+  data.days[$eventForm.day.value].push(formData);
 }
 
 $eventForm.addEventListener('submit', handleAddEntry);
